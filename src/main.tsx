11c96625b0079607f3873c06de59bd4725a6af16
Noel Hoppe <noel.hoppe@gmx.de>
2025-02-12 22:07:34 +0100

    styling game

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/modern-normalize.css'
+import './styles/style.css'
+import './styles/utils.css' /* utility classes have the highest priority */
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
